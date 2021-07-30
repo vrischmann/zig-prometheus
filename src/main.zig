c472215e3ea6cccdf40609479b3bd735cbd0a5f9
Vincent Rischmann <vincent@rischmann.fr>
2021-07-31 01:08:48 +0200

    simplify registry, implement writePrometheus

--- src/main.zig
+++ src/main.zig
@@ -1,28 +1,11 @@
 const std = @import("std");
 const fmt = std.fmt;
+const hash_map = std.hash_map;
 const heap = std.heap;
 const mem = std.mem;
 const testing = std.testing;
 
 pub const Counter = @import("Counter.zig");
-
-fn MetricMap(comptime Value: type) type {
-    return struct {
-        const Self = @This();
-
-        const MapType = std.hash_map.StringHashMapUnmanaged(Value);
-
-        map: MapType = .{},
-
-        pub fn init() Self {
-            return Self{};
-        }
-
-        pub fn deinit(self: *Self, allocator: *mem.Allocator) void {
-            self.map.deinit(allocator);
-        }
-    };
-}
 
 pub const GetCounterError = error{
     NameTooLong,
@@ -34,26 +17,28 @@ const max_name_length = 1024;
 pub const Registry = struct {
     const Self = @This();
 
-    const CounterMap = MetricMap(Counter);
+    const CounterMap = hash_map.StringHashMapUnmanaged(Counter);
 
-    arena: heap.ArenaAllocator,
     allocator: *mem.Allocator,
 
     mutex: std.Thread.Mutex,
     counters: CounterMap,
 
-    pub fn init(self: *Self, allocator: *mem.Allocator) !void {
+    pub fn create(allocator: *mem.Allocator) !*Self {
+        const self = try allocator.create(Self);
+
         self.* = .{
-            .allocator = undefined,
-            .arena = heap.ArenaAllocator.init(allocator),
+            .allocator = allocator,
             .mutex = .{},
-            .counters = CounterMap.init(),
+            .counters = CounterMap{},
         };
-        self.allocator = &self.arena.allocator;
+
+        return self;
     }
 
-    pub fn deinit(self: *Self) void {
-        self.arena.deinit();
+    pub fn destroy(self: *Self) void {
+        self.counters.deinit(self.allocator);
+        self.allocator.destroy(self);
     }
 
     pub fn getOrCreateCounter(self: *Self, comptime format: []const u8, values: anytype) GetCounterError!*Counter {
@@ -64,28 +49,58 @@ pub const Registry = struct {
 
         const name = try fmt.allocPrint(self.allocator, format, values);
 
-        var gop = try self.counters.map.getOrPut(self.allocator, name);
+        var gop = try self.counters.getOrPut(self.allocator, name);
         if (!gop.found_existing) {
             gop.value_ptr.* = .{};
         }
         return gop.value_ptr;
     }
 
-    pub fn writePrometheus(writer: anytype) !void {
-        _ = writer;
+    pub fn writePrometheus(self: *Self, allocator: *mem.Allocator, writer: anytype) !void {
+        var arena = heap.ArenaAllocator.init(allocator);
+        defer arena.deinit();
+
+        try writePrometheusMetrics(&arena.allocator, CounterMap, self.counters, writer);
+    }
+
+    fn writePrometheusMetrics(allocator: *mem.Allocator, comptime MapType: type, map: MapType, writer: anytype) !void {
+        // Get the keys, sorted
+        const keys = blk: {
+            var key_list = try std.ArrayList([]const u8).initCapacity(allocator, map.count());
+
+            var key_iter = map.keyIterator();
+            while (key_iter.next()) |key| {
+                key_list.appendAssumeCapacity(key.*);
+            }
+
+            break :blk key_list.toOwnedSlice();
+        };
+
+        std.sort.sort([]const u8, keys, {}, stringLessThan);
+
+        // Write each metric in key order
+        for (keys) |key, i| {
+            const value = map.get(key) orelse unreachable;
+
+            try value.writePrometheus(writer, key);
+
+            if (i + 1 < keys.len) {
+                try writer.writeAll("\n");
+            }
+        }
     }
 };
 
-var DefaultRegistry: Registry = undefined;
-
-pub fn getOrCreateCounter(comptime format: []const u8, values: anytype) GetCounterError!*Counter {
-    return DefaultRegistry.getOrCreateCounter(format, values);
+fn stringLessThan(context: void, lhs: []const u8, rhs: []const u8) bool {
+    _ = context;
+    return mem.lessThan(u8, lhs, rhs);
 }
 
 test "registry getOrCreateCounter" {
-    var registry: Registry = undefined;
-    try registry.init(testing.allocator);
-    defer registry.deinit();
+    var arena = heap.ArenaAllocator.init(testing.allocator);
+    defer arena.deinit();
+    var registry = try Registry.create(&arena.allocator);
+    defer registry.destroy();
 
     const key = "http_requests{{status=\"{d}\"}}";
 
@@ -99,15 +114,30 @@ test "registry getOrCreateCounter" {
     try testing.expectEqual(@as(u64, 10), counter.get());
 }
 
-test "getOrCreateCounter" {
-    try DefaultRegistry.init(testing.allocator);
-    defer DefaultRegistry.deinit();
+test "registry writePrometheus" {
+    var arena = heap.ArenaAllocator.init(testing.allocator);
+    defer arena.deinit();
+    var registry = try Registry.create(&arena.allocator);
+    defer registry.destroy();
 
-    var counter = try getOrCreateCounter("http_requests", .{});
-    counter.inc();
+    var i: usize = 0;
+    while (i < 3) : (i += 1) {
+        var counter = try registry.getOrCreateCounter("http_requests{d}", .{i});
+        counter.set(i * 2);
+    }
 
-    counter = try getOrCreateCounter("http_requests", .{});
-    try testing.expectEqual(@as(u64, 1), counter.get());
+    var buffer = std.ArrayList(u8).init(std.testing.allocator);
+    defer buffer.deinit();
+
+    try registry.writePrometheus(std.testing.allocator, buffer.writer());
+
+    const exp =
+        \\http_requests0 0
+        \\http_requests1 2
+        \\http_requests2 4
+    ;
+
+    try testing.expectEqualStrings(exp, buffer.items);
 }
 
 test "" {
